c6f1ee80632416711e7e8bce73b3aea23bb4be41
daily sergey <guskov13sv@gmail.com>
2020-08-13 23:33:30 +0300

    added template

--- src/dailydata/views.py
+++ src/dailydata/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Vacancy, Language, City
 
 # Create your views here.
+
+
+def home_view(request):
+
+    qs = Vacancy.objects.all()
+    return render(request, "home.html", {'object_list': qs})
